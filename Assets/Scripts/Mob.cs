2316b2ab9d18cd98b69de9b618075367f9dbe0b0
Matthew Conto <mbsconto@gmail.com>
2015-04-09 16:46:26 -0400

    init

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob : MonoBehaviour {
+
+	public float walkSpeed;
+	public float runSpeed;
+
+	public void turnToFace( Transform target ) {
+
+	}
+
+	public void walk() {
+
+	}
+
+	public void run() {
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
